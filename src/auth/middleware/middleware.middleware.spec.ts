662517293dd0f2ffd1a59f9c934f5494d449bbb0
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-03-18 12:41:18 +0545

    feat(auth): remove auth middleware

--- src/auth/middleware/middleware.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MiddlewareMiddleware } from './middleware.middleware';
-
-describe('MiddlewareMiddleware', () => {
-  it('should be defined', () => {
-    expect(new MiddlewareMiddleware()).toBeDefined();
-  });
-});
